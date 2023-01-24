b6f978c2c48995f18aa7f32ad45cbfddbde7ec1f
nick benanti <nickbenanti324@gmail.com>
2023-01-24 13:12:54 -0600

    complete coding challenge

---
+++ maxOfTwo.js
@@ -0,0 +1,10 @@
+let x = 1;
+let y = 2;
+
+if(x > y){
+    console.log(`the largest value is ${x}`)   
+} else if (y > x ){
+    console.log (`The largest  value is ${y}`)
+} else {
+    console.log(` the values, ${y} and {x}, are identical.`) 
+}
